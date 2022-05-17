4672ec4b2289c77d1d88f6de09c4c1e6875ade68
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-05-18 00:18:18 +0100

    Update pagination logic

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -6,6 +6,7 @@ import Search from "../components/Search.vue";
 import { useState } from "../store/store";
 import { computed } from "vue";
 import Pagination from "../components/Pagination.vue";
+import handlePagination from "../composable/usePagination";
 
 export default {
   components: {
@@ -20,8 +21,11 @@ export default {
       state.filters.length ? state.filters : state.products
     );
 
+    const handlePaginationValue = handlePagination(products.value);
+
     return {
-      products,
+      // products,
+      ...handlePaginationValue,
     };
   },
 };
@@ -38,13 +42,46 @@ export default {
         <div class="hide-print">
           <div class="flex flex-wrap">
             <Product
-              v-for="(product, index) of products"
+              v-for="(product, index) of paginatedData"
               :key="index"
               :data="product"
             />
           </div>
           <div>
-            <Pagination />
+            <!-- <Pagination :data="perPage" /> -->
+
+            <div class="mt-10">
+              <nav aria-label="Page navigation example">
+                <ul class="inline-flex -space-x-px">
+                  <li>
+                    <button
+                      @click="backPage"
+                      class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      Previous
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      v-for="item in Math.ceil(data.length / perPage)"
+                      :key="item"
+                      @click="() => goToPage(item)"
+                      class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      {{ item }}
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      @click="nextPage"
+                      class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      Next
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
         <Checkout />
